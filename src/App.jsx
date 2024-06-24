978992a78050637d8cc9a64a9e8694d2c4d112be
João Vitral <joaoviniciusvitral@hotmail.com>
2024-06-24 20:03:58 -0300

    feat: Add lazy loading for images on homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
 function App() {
   return (
-    <>
-      <iframe src="/pdf.pdf"></iframe>
-    </>
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      {Array.from({ length: 37 }, (_, idx) => idx + 1).map((image, index) => {
+        console.log(image)
+        const formattedIndex = image < 10 ? `0${image}` : image
+        return (
+          <img
+            key={index}
+            src={`/images/_NOIVAS_2024_VIERT_page-00${formattedIndex}.jpg`}
+            alt={`Imagem ${index + 1}`}
+            style={{
+              width: "100vw",
+              flexShrink: 0,
+              aspectRatio: "2/3",
+              backgroundColor: "gray",
+            }}
+          />
+        )
+      })}
+    </div>
   )
 }
 
